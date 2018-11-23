001e961e7b8c2782554328ba5a0f7c8509e375c1
Axillarator <axel@gruenert-mz.de>
2018-11-23 17:39:29 +0100

    cleanup input text file rover class structure

--- src/main/rover/App.kt
+++ src/main/rover/App.kt
@@ -1,7 +1,69 @@
 package rover
 
-class App{
+import java.io.File
+import javax.print.attribute.IntegerSyntax
+
+class App {
     fun main(args: Array<String>) {
-        println("Hello World")
+        //read the input from file
+        //for each rover
+        //  perform movement
+        //  print output
     }
+
+    fun inputL(): String {
+        println("L")
+        return "L"
+    }
+
+    fun userInput(): String {
+        val input: String?
+        input = readLine()
+        return input ?: ""
+    }
+
+    fun readInput(){
+        val fileName = "src/main/rover/input.txt"
+        val content = File(fileName).readText()
+        println(content)
+    }
+}
+
+class Rover(x_start: Int, y_start: Int, heading: String){
+
+    var x = x_start
+    var y = y_start
+    var facing = heading
+
+    fun move(input: String){
+        /**
+         * moves the rover by changing the variables according to the input
+         */
+        if (input.equals("M")) {
+            when (facing) {
+                "N" -> y += 1
+                "S" -> y -= 1
+                "E" -> x += 1
+                "W" -> x -= 1
+            }
+        }
+        if (input.equals("L")){
+            when (facing){
+                "N" -> facing = "W"
+                "S" -> facing = "E"
+                "E" -> facing = "N"
+                "W" -> facing = "S"
+            }
+        }
+        if (input.equals("R")){
+            when (facing){
+                "N" -> facing = "E"
+                "S" -> facing = "W"
+                "E" -> facing = "S"
+                "W" -> facing = "N"
+            }
+
+        }
+    }
+
 }
--- src/test/rover/AppTest.kt
+++ src/test/rover/AppTest.kt
@@ -7,12 +7,54 @@ import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 internal class AppTest {
 
     @Test
-    fun itShouldRunTests() {
+    fun runTests() {
         assertThat(1).isEqualTo(1)
     }
 
     @Test
-    fun itShouldRun2ndTest() {
-        assertThat(2+1).isEqualTo(3)
+    fun createInstanceFromAppClass() {
+        val appInstance = rover.App()
     }
+
+    @Test
+    fun inputLTest() {
+        val appInstance = rover.App()
+        assertThat(appInstance.inputL()).isEqualTo("L")
+    }
+
+    @Test
+    fun userInput() {
+        val appInstance = rover.App()
+        appInstance.readInput()
+    }
+
+    @Test
+    fun createRoverInstance(){
+        val rover = rover.Rover(1, 1, "N")
+    }
+
+    @Test
+    fun moveRover(){
+        val rover = rover.Rover(1, 1, "N")
+        rover.move("M")
+        assertThat(rover.x).isEqualTo(1)
+        assertThat(rover.y).isEqualTo(2)
+        assertThat(rover.facing).isEqualTo("N")
+
+        rover.move("L")
+        assertThat(rover.x).isEqualTo(1)
+        assertThat(rover.y).isEqualTo(2)
+        assertThat(rover.facing).isEqualTo("W")
+
+        rover.move("M")
+        assertThat(rover.x).isEqualTo(0)
+        assertThat(rover.y).isEqualTo(2)
+        assertThat(rover.facing).isEqualTo("W")
+
+        rover.move("R")
+        assertThat(rover.x).isEqualTo(0)
+        assertThat(rover.y).isEqualTo(2)
+        assertThat(rover.facing).isEqualTo("N")
+    }
+
 }
